0c3434c94031ec723eba96a4f70f41e4dec5869a
ACER <anhtuquan.bd@gmail.com>
2026-05-22 21:33:06 +0700

    Connect frontend to render backend

--- frontend/src/pages/AICoach.jsx
+++ frontend/src/pages/AICoach.jsx
@@ -27,7 +27,7 @@ export default function AICoach({ onNavigate, onLogout }) {
     setLoading(true);
 
     try {
-  const res = await fetch("http://localhost:5000/api/chat", {
+  const res = await fetch("https://fitness-ai-app-71hw.onrender.com/api/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
