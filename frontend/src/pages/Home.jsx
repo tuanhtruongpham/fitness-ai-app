25e12c2747c76c1b24d596a1304bf2048060068d
ACER <anhtuquan.bd@gmail.com>
2026-05-25 23:27:45 +0700

    new

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,6 @@ function Home({ onNavigate, onLogout }) {
       const token = localStorage.getItem("token");
 
       const res = await axios.get("https://fitness-ai-app-71hw.onrender.com/api/dashboard", {
-     const res = await axios.get("https://fitness-ai-app-71hw.onrender.com/api/dashboard", {
   headers: {
     authorization: token,
   },
