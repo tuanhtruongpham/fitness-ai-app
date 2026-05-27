92c798ec79e4b32b26a83bc806fd793bba62d436
tinuytktk <tranuy798@gmail.com>
2026-05-27 20:48:21 +0700

    fix gmail smtp email service

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -5,16 +5,22 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   family: 4,
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS?.replace(/\s/g, ""),
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
 
 const sendEmail = async ({ to, subject, html }) => {
+  console.log("📧 START SEND EMAIL TO:", to);
+  console.log("📧 EMAIL USER:", process.env.EMAIL_USER);
+  console.log("📧 HAS EMAIL PASS:", !!process.env.EMAIL_PASS);
+
   await transporter.sendMail({
     from: `"Fitness AI App" <${process.env.EMAIL_USER}>`,
     to,
