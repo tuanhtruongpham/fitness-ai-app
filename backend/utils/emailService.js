d3ded51c47db9cc6d8f32c09b35c76f478d4cd01
tinuytktk <tranuy798@gmail.com>
2026-05-27 20:23:22 +0700

    fix gmail smtp ipv4

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,17 +1,20 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS?.replace(/\s/g, ""),
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const sendEmail = async ({ to, subject, html }) => {
-  console.log("📧 EMAIL USER:", process.env.EMAIL_USER);
-  console.log("📧 EMAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
-
   await transporter.sendMail({
     from: `"Fitness AI App" <${process.env.EMAIL_USER}>`,
     to,
